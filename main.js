c7d64e9cf31d5a6b7771e002f800715d239b844b
Max Martinsen <max.martinsen@hotmail.com>
2023-11-23 19:30:56 +0100

    Created tag function and added Authentication buttons

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import './src/index.css';
 import 'flowbite';
 import './src/css/iconsfont.css';
+
+import { toggleTabs } from './src/js/components/components.mjs';
+
+document.addEventListener('DOMContentLoaded', () => {
+  toggleTabs('.tab-trigger', '.tab-content');
+});
