0b1dfbad2d3e83e8c95a7ed07be8f0d502d37ee8
Max Martinsen <max.martinsen@hotmail.com>
2023-12-03 14:04:24 +0100

    Listing and listing validation

--- src/js/form/listing/handleCreateListing.mjs
+++ src/js/form/listing/handleCreateListing.mjs
@@ -10,20 +10,72 @@ export function handleCreateListing() {
   const submitButton = document.getElementById('listing-submit');
   const progressButton = document.getElementById('listing-progress');
   const successButton = document.getElementById('listing-success');
+  const addButton = document.getElementById('listing-image-add');
+  const imageInput = document.getElementById('listing-image');
+  const imageList = document.getElementById('listing-image-added');
+  let mediaGallery = [];
+
+  addButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const imageUrl = imageInput.value;
+    if (imageUrl) {
+      mediaGallery.push(imageUrl);
+      imageList.textContent = mediaGallery.join(', ');
+      imageInput.value = '';
+    }
+  });
 
   if (!form || !submitButton || !progressButton || !successButton) return;
 
   submitButton.addEventListener('click', async (event) => {
     event.preventDefault();
-    submitButton.classList.add('hidden');
-    progressButton.classList.remove('hidden');
-    const accessToken = localStorage.getItem('accessToken');
     const title = document.getElementById('listing-title').value;
     const during = document.getElementById('listing-during').value;
-    const image = document.getElementById('listing-image').value;
     const description = document.getElementById('listing-description').value;
     const tagifyTags = document.getElementById('listing-tags').value;
-    const tags = JSON.parse(tagifyTags).map((tagObject) => tagObject.value);
+
+    let tags = [];
+    try {
+      tags = JSON.parse(tagifyTags).map((tagObject) => tagObject.value);
+    } catch (error) {
+      console.error('Error parsing tags JSON:', error);
+      // Handle the error, display a message, or take appropriate action.
+      return;
+    }
+
+    // Check if imageInput is not empty and automatically add the image URL
+    const imageInputValue = imageInput.value;
+    if (imageInputValue.trim() !== '') {
+      mediaGallery.push(imageInputValue);
+      imageList.textContent = mediaGallery.join(', ');
+      imageInput.value = '';
+    }
+
+    // Title Validation
+    if (title.length < 3) {
+      alert('Title must be at least 3 characters long.');
+      submitButton.classList.remove('hidden');
+      return;
+    }
+
+    // Description Validation
+    if (description.length > 199) {
+      alert('Description must be 199 characters or less.');
+      submitButton.classList.remove('hidden');
+      return;
+    }
+
+    // End Date Validation
+    if (during === '' || during === 'During') {
+      alert('Please select a valid end date for the auction.');
+      submitButton.classList.remove('hidden');
+      return;
+    }
+
+    submitButton.classList.add('hidden');
+    progressButton.classList.remove('hidden');
+    progressButton.classList.add('inline-flex');
+    const accessToken = localStorage.getItem('accessToken');
 
     const endsAt = calculateEndDate(during);
 
@@ -31,7 +83,7 @@ export function handleCreateListing() {
       title,
       description,
       tags,
-      media: [image],
+      media: mediaGallery,
       endsAt,
     };
 
@@ -45,11 +97,16 @@ export function handleCreateListing() {
 
       // Change to success button and close modal after a delay
       setTimeout(() => {
+        progressButton.classList.remove('inline-flex');
         progressButton.classList.add('hidden');
         successButton.classList.remove('hidden');
+        successButton.classList.add('inline-flex');
 
-        // Close modal after additional delay
+        // Clear the form fields and mediaGallery for the next listing
         setTimeout(() => {
+          successButton.classList.add('hidden');
+          submitButton.classList.remove('hidden');
+          clearFormFields();
           document.querySelector('[data-modal-hide="listing-modal"]').click();
         }, 1500);
       }, 2000);
@@ -59,20 +116,31 @@ export function handleCreateListing() {
       submitButton.classList.remove('hidden');
     }
   });
+
+  // Function to clear form fields and mediaGallery
+  function clearFormFields() {
+    document.getElementById('listing-title').value = '';
+    document.getElementById('listing-during').value = '';
+    document.getElementById('listing-description').value = '';
+    document.getElementById('listing-tags').value = 'Sale, Auction, Bid';
+    imageInput.value = '';
+    mediaGallery = [];
+    imageList.textContent = 'Not added';
+  }
 }
 
 function calculateEndDate(during) {
   const now = new Date();
   switch (during) {
     case '3d':
-      return new Date(now.setDate(now.getDate() + 3)).toISOString();
+      return new Date(now.getTime() + 3 * 24 * 60 * 60 * 1000).toISOString();
     case '5d':
-      return new Date(now.setDate(now.getDate() + 5)).toISOString();
+      return new Date(now.getTime() + 5 * 24 * 60 * 60 * 1000).toISOString();
     case '7d':
-      return new Date(now.setDate(now.getDate() + 7)).toISOString();
+      return new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000).toISOString();
     case '30d':
-      return new Date(now.setDate(now.getDate() + 30)).toISOString();
+      return new Date(now.getTime() + 30 * 24 * 60 * 60 * 1000).toISOString();
     default:
-      return new Date(now.setDate(now.getDate() + 7)).toISOString();
+      return new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000).toISOString();
   }
 }
